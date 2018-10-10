0ba2531c52d81903230a1d582715116b8e161159
Ahuilaca <santosrodriguez.rcs@gmail.com>
2018-10-09 20:33:34 -0700

    first save

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+//This array will display computers random number from 19-120. Buttons from 1-12
+//var buttonNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+//var buttonNumber = Math.floor(Math.random() * 12) + 1;
+//var computerNumber = [" "];
+//var computerNumber = Math.floor(Math.random() * 12) + 1;
+
+$(".crystal-button").on("click", function() {
+    alert("test");
+})
+
+//This is the win/loss score card
+
+//Here are the crystal buttons
+
+//Total score holder
